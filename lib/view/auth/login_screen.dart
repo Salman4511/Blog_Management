a1a8cc26dbeaaf6aef2d8c85a2796e71d20cdb9e
Salmanulfaris.k <salmanulfaris4511@gmail.com>
2024-08-23 15:35:39 +0530

    Base model completed

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -1,92 +1,12 @@
 
-// import 'package:blog_management_app/controller/auth_controller.dart';
-// import 'package:blog_management_app/utils/validators.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-
-// class LoginScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final _emailController = TextEditingController();
-//     final _passwordController = TextEditingController();
-//     final _formKey = GlobalKey<FormState>();
-
-//     void _showError(String message) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text(message)),
-//       );
-//     }
-
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Login')),
-//       body: Padding(
-//         padding: EdgeInsets.all(16.0),
-//         child: Form(
-//           key: _formKey,
-//           child: Column(
-//             children: [
-//               TextFormField(
-//                 controller: _emailController,
-//                 decoration: InputDecoration(labelText: 'Email'),
-//                 keyboardType: TextInputType.emailAddress,
-//                 validator: Validators.validateEmail,
-//               ),
-//               TextFormField(
-//                 controller: _passwordController,
-//                 decoration: InputDecoration(labelText: 'Password'),
-//                 obscureText: true,
-//                 validator: Validators.validatePassword,
-//               ),
-//               SizedBox(height: 20),
-//               ElevatedButton(
-//                 onPressed: () async {
-//                   if (_formKey.currentState?.validate() ?? false) {
-//                     final email = _emailController.text;
-//                     final password = _passwordController.text;
-
-// ignore_for_file: use_build_context_synchronously
-
-//                     try {
-//                       await context
-//                           .read<AuthController>()
-//                           .signIn(email, password);
-//                       if (context.read<AuthController>().user != null) {
-//                         Navigator.pushReplacementNamed(context, '/home');
-//                       } else {
-//                         _showError(
-//                             'Login failed. Please check your credentials.');
-//                       }
-//                     } catch (e) {
-//                       _showError('An error occurred: $e');
-//                     }
-//                   }
-//                 },
-//                 child: Text('Login'),
-//               ),
-//               Row(
-//                 children: [
-//                   Text("Don't have an account?"),
-//                   TextButton(
-//                     onPressed: () {
-//                       Navigator.pushNamed(context, '/signup');
-//                     },
-//                     child: Text('Sign Up'),
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:blog_management_app/controller/auth_controller.dart';
 import 'package:blog_management_app/utils/validators.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final _emailController = TextEditingController();
@@ -113,7 +33,6 @@ class LoginScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                // Logo or App Name
                 const Icon(
                   Icons.person,
                   size: 100,
@@ -134,7 +53,6 @@ class LoginScreen extends StatelessWidget {
                   style: TextStyle(fontSize: 16, color: Colors.grey),
                 ),
                 const SizedBox(height: 40),
-                // Email Field
                 TextFormField(
                   controller: _emailController,
                   decoration: InputDecoration(
@@ -148,7 +66,6 @@ class LoginScreen extends StatelessWidget {
                   validator: Validators.validateEmail,
                 ),
                 const SizedBox(height: 20),
-                // Password Field
                 TextFormField(
                   controller: _passwordController,
                   decoration: InputDecoration(
@@ -162,7 +79,6 @@ class LoginScreen extends StatelessWidget {
                   validator: Validators.validatePassword,
                 ),
                 const SizedBox(height: 20),
-                // Login Button
                 ElevatedButton(
                   onPressed: () async {
                     if (_formKey.currentState?.validate() ?? false) {
@@ -196,7 +112,6 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 20),
-                // Sign Up Link
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
